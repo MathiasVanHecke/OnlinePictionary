8d37b3d7643cade60d1c770fbb9982f03158dae9
MathiasVanHecke <mathias.van.hecke@student.howest.be>
2018-11-09 12:02:26 +0100

    Voorkomen zelfmoord

---
+++ draw-together/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="o-container">
+    <HelloWorld msg="The login page"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'login',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './src/style/objects/objects.container';
+</style>
