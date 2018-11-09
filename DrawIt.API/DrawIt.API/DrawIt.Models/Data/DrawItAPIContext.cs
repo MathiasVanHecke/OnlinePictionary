db2c437f67f7a040ab04a6f8221cef34d658cd79
MathiasVanHecke <mathias.van.hecke@student.howest.be>
2018-11-09 12:03:51 +0100

    Toevoeging api

---
+++ DrawIt.API/DrawIt.API/DrawIt.Models/Data/DrawItAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DrawIt.Models.Data;
+
+namespace DrawIt.API.Models
+{
+    public class DrawItAPIContext : DbContext
+    {
+        public DrawItAPIContext (DbContextOptions<DrawItAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DrawIt.Models.Data.Word> Word { get; set; }
+    }
+}
