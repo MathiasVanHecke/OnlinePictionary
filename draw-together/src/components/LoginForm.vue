ffdd4f95824ff5331140848a7e4382868593633b
Unknown <celine.abbeloos@student.howest.be>
2018-11-14 23:45:54 +0100

    ridiculously fancy fancycheckboxes added

--- draw-together/src/components/LoginForm.vue
+++ draw-together/src/components/LoginForm.vue
@@ -1,27 +1,37 @@
 <template>
   <div v-if="isLogin" class="c-login o-smallpage">
     <h1 class="o-smallpage-header">Log In</h1>
-    <input type="text" class="c-login-input" placeholder="Username">
-    <input type="password" class="c-login-input" placeholder="Password">
+    <input v-model="username" type="text" class="c-login-input" placeholder="Username">
+    <input v-model="password" type="password" class="c-login-input" placeholder="Password">
     <button class="c-login-button">Log In</button>
     <a class="c-login-link" v-on:click="switchLogin">Don't have an account yet?</a>
   </div>
   <div v-else class="c-login o-smallpage">
     <h1 class="o-smallpage-header">Register</h1>
-    <input type="text" class="c-login-input" placeholder="Username">
-    <input type="email" class="c-login-input" placeholder="Email">
-    <input type="password" class="c-login-input" placeholder="Password">
-    <input type="password" class="c-login-input" placeholder="Confirm password">
+    <input v-model="username" type="text" class="c-login-input" placeholder="Username">
+    <input v-model="email" type="email" class="c-login-input" placeholder="Email">
+    <input v-model="password" type="password" class="c-login-input" placeholder="Password">
+    <input v-model="password2" type="password" class="c-login-input" placeholder="Confirm password">
+    <span class="c-login-tac"><FancyCheckbox/><p class="c-login-tac-text">I agree with the terms and conditions.</p></span>
     <button class="c-login-button">Register</button>
     <a class="c-login-link" v-on:click="switchLogin">Already have an account?</a>
   </div>
 </template>
 
 <script>
+import FancyCheckbox from '@/components/FancyCheckbox.vue'
+
 export default {
   name: 'AppHeader',
+  components : {
+    FancyCheckbox,
+  },
   data() {
     return {
+      username : '',
+      email : '',
+      password: '',
+      password2: '',
       isLogin : false,
     }
   },
