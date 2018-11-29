9bcf7e681e81d9d817de4c636bca07859df89566
Unknown <celine.abbeloos@student.howest.be>
2018-11-29 19:51:54 +0100

    service worker madness

--- draw-together/dist/serviceWorker.js
+++ draw-together/dist/serviceWorker.js
@@ -1,5 +1,18 @@
-importScripts("/precache-manifest.45fc99f3e8e925bd07b9ca524fdb6123.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.bf574f2706f29added430d43560cb671.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('install', function (){
     console.log('service worker installed at', new Date().toLocaleTimeString());
+})
+
+self.addEventListener('activate', function (){
+    console.log('service worker activated at', new Date().toLocaleTimeString());
+})
+
+self.addEventListener('fetch', function(event){
+    if(!navigator.onLine){
+        event.respondWith(new Response('<h1>You seem to be offline...</h1>'))
+    }
+    else {
+        event.respondWith(fetch(event.request));
+    }
 })
